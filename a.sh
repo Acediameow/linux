a75ab2318834569b41110133daed9fc9d3d7230a
Acediameow <2327688387@qq.com>
2020-03-31 18:11:22 +0800

    2020-3-31 add profile and bashrc

--- a.sh
+++ a.sh
@@ -6,12 +6,31 @@ SELFDIR = $(dirname"$0")
 cd "$SELFDIR"
 
 #初始化vim配置
+echo"配置vim..."
 cp -R HOME_vim ~/.vim
 cp vimrc ~/.vimrc
+echo"vim配置成功！"
 
 #复制bash配置文件
+echo"配置bash..."
 cp profile ~/.profile
 cp bashrc ~/. bahsrc
+echo"bahs配置成功"
 
 #初始化nano配置
+echo"初始化nano"
 cp nanorc ~/.nanorc
+echo"初始化nano成功"
+
+#创建目录
+echo"创建目录.."
+DIRLIST = "bin c sh tmp"
+for d in $DIRLIST ; do
+
+cd
+
+	if [ ! -d "$d" ] ; then
+		mkdir "$d"
+	fi
+done
+
